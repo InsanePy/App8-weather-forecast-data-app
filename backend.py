3b4d64ececfe4f13dca92bf82d5f8c389f8359c1
StudioCrew-design <dev.studiocrew@gmail.com>
2023-06-30 04:49:08 +0530

    Day 33: Plot temperatures and sky data

---
+++ backend.py
@@ -0,0 +1,18 @@
+import requests
+#API_KEY= "ce8207f61f82de03b1c890ee0a91d4ad" Not activated yet
+
+API_KEY="141710af2113bab9f55ef73e1bcd33d5"
+
+
+def get_data(place, forecast_days):
+    url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    return filtered_data
+
+
+if __name__ == "__main__":
+    print(get_data("Tokyo", 3))
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place")
@@ -7,14 +8,18 @@ days = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the num
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
+if place:
+    # Get temparature and sky data
+    filtered_data = get_data(place, days)
+    if option == "Temperature":
+        temperatures = [item["main"]["temp"] for item in filtered_data]
+        dates = [item["dt_txt"] for item in filtered_data]
+        figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+        st.plotly_chart(figure)
 
-def get_data(days):
-    dates = ["2022-25-10", "2022-26-10", "2022-27 -10"]
-    temperatures = [10, 11, 15]
-    temperatures = [days * i for i in temperatures]
-    return dates, temperatures
-
-
-d, t = get_data(days)
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"} )
-st.plotly_chart(figure)
+    if option == "Sky":
+        sky_conditions = [item["weather"][0]["main"] for item in filtered_data]
+        images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                  "Rain": "images/rain.png", "Snow":"images/snow.png"}
+        image_paths = [images[item] for item in sky_conditions]
+        st.image(image_paths, width=115)
